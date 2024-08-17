b7fa788dae5dbd59cfe62f3e6204f2fbb44567a9
yagisan <shoyaaoyagi@gmail.com>
2024-08-17 15:06:17 +0900

    Create user registration form and User DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+    
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    if @user.save
+      redirect_to("/")
+    else
+      render("users/new")
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get "users/index" => "users#index"
 
-  get "home/top" => "home#top"
+  post "users/create" => "users#create"
+  get "users/index" => "users#index"
+  get "users/signup" => "users#new"
+
+  get "/" => "home#top"
+
+
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
