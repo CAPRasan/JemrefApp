92d2178fa6c21001242994a402c857b133890b7b
yagisan <shoyaaoyagi@gmail.com>
2024-08-17 14:18:19 +0900

    Create user index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  get "users/index" => "users#index"
+
   get "home/top" => "home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
