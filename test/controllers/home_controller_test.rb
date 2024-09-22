0334ce578c729f901c79afe207bcc45ff21efd68
yagisan <shoyaaoyagi@gmail.com>
2024-09-22 18:49:16 +0900

    create title helper

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,6 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | JemRef"
+    assert_select "title", "ホーム | JemRef"
   end
 end
