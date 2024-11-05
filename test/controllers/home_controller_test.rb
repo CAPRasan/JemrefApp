eee31d88e06432b858d1010b22dc791e31025a69
yagisan <shoyaaoyagi@gmail.com>
2024-11-05 18:00:04 +0900

    homeページのテストの追加

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,18 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "ホーム | JemRef"
   end
+
+  test "should get about" do
+    get home_about_path
+    assert_response :success
+    assert_template "home/about"
+    assert_select "title", "使い方 | JemRef"
+  end
+
+  test "should get information" do
+    get home_information_path
+    assert_response :success
+    assert_template "home/information"
+    assert_select "title", "お知らせ・お問い合わせ | JemRef"
+  end
 end
