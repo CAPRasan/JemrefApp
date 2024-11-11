9fd9be4784da8572ad07b4d61308a8104c2f03a6
yagisan <shoyaaoyagi@gmail.com>
2024-11-11 21:28:20 +0900

    未使用アクションの削除（コントローラ）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   #   @users = User.all
   # end
 
-  def show
-    @user = User.find_by(id: params[:id])
-  end
+  # def show
+  #   @user = User.find_by(id: params[:id])
+  # end
 
   def new
     @user = User.new
