d3629ebc14c8baf0632a4bba8dcb00c7e524e5e5
yagisan <shoyaaoyagi@gmail.com>
2024-11-12 08:56:09 +0900

    sessionsコントローラテストの拡充

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,3 +6,26 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 end
+
+class InvalidSessionsTest < SessionsControllerTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "should redirect login with logged-in user" do
+    log_in_as(@user)
+    get login_path
+    assert_redirected_to records_url
+  end
+
+  test "should redirect new with logged-in user" do
+    log_in_as(@user)
+    get sessions_new_path
+    assert_redirected_to records_url
+  end
+
+  test "should redirect destroy when not logged-in user" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
