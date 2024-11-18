bde5db97abd2e8504fee3067e61cc5014d29ddc6
yagisan <shoyaaoyagi@gmail.com>
2024-11-18 13:00:26 +0900

    サンプル投入ロジックの切り分け

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,16 +1,17 @@
 class RecordsController < ApplicationController
+  include RecordsHelper
   before_action :logged_in_user
   before_action :ensure_correct_user, { only: [ :edit, :update, :destroy ] }
 
   def index
-    # キーワード検索とタグ検索は別機能（ややfat）。
+    # 通常検索とタグ検索は別機能。
     # TODO: strong param設定。
     @q = current_user.records.ransack(search_params)
-    @tag_name = tag_params ? tag_params[:tag_name] : nil
+    @tag_name = tag_params[:tag_name].presence
     # タグ検索の場合。ソート機能は後日実装
     records = if @tag_name.present?
                  Record.tagged_with(@tag_name, current_user).order(:publish_date)
-
+    # 通常検索の場合
     else @q.result(distinct: true).order(:publish_date)
     end
     @records = records.paginate(page: params[:page], per_page: 20)
@@ -56,31 +57,7 @@ class RecordsController < ApplicationController
   end
 
   def create_sample
-    sample_records = [
-    { type: "Book", author_name: "夏目漱石", main_title: "文学論",
-      sub_title: "", publish_date: 1907, publisher: "大倉書店",
-      status: "read", memo: "削除・更新は右のボタンを押してください。", tags: "夏目漱石,文学,明治" },
-    { type: "Book", author_name: "三木清", main_title: "哲学ノート",
-      sub_title: "", publish_date: 1957, publisher: "新潮社",
-      status: "read", memo: "タグはこちらに表示されます。タグをクリックすると、同じタグをもつ文献情報に絞り込みができます。", tags: "三木清,哲学,昭和" },
-    { type: "Book", author_name: "伊沢修二", main_title: "教育学",
-      sub_title: "", publish_date: 1883, publisher: "丸善商社",
-      status: "unread", memo: "これはメモです。", tags: "伊沢修二,教育学,明治,ブリッジウォーター師範学校" },
-    { type: "Paper", author_name: "牛山充", main_title: "ミンストレル雑考(一）", sub_title: "",
-      publish_date: 1930, publisher: "音楽世界社", compiled_by: "",
-      status: "read", publication_main_title: "音楽世界", publication_sub_title: "",
-      volume: 2, no: 3, memo: "文献を新しく登録する場合は、ページ最上部の「文献情報の入力」をクリックしてください。", tags: "音楽評論家,音楽雑誌,昭和,音楽史" },
-    { type: "Compilation", author_name: "増沢健美", main_title: "音楽史", sub_title: "",
-      publish_date: 1927, publisher: "アルス", compiled_by: "小松耕輔（主幹）",
-      status: "read", publication_main_title: "アルス西洋音楽講座", publication_sub_title: "",
-      volume: 2, memo: "こちらのサンプルは、不要になりましたら削除してください。", tags: "音楽史,1920年代,昭和,音楽評論家" }
-    ]
-    sample_records.each do |record_data|
-      tags_array_sample = extract_tags(record_data.delete(:tags))  # タグを配列に変換、レコードデータから不要なtagsキーを削除
-      record = current_user.records.create!(record_data)           # レコードを作成
-      record.save_tags(tags_array_sample)                          # タグを保存
-    end
-
+    SampleRecordsCreater.new(current_user).call
     flash[:info] = "サンプルデータを追加しました"
     redirect_to records_path
   end
@@ -110,7 +87,7 @@ class RecordsController < ApplicationController
         :main_title_cont,
         # 人名で検索
         :author_name_or_compiled_by_cont,
-        # 状態で検索
+        # 既読・未読・不必要で絞り込み
         :status_eq
     ])
       else
@@ -121,11 +98,6 @@ class RecordsController < ApplicationController
     # tag用のparams処理
     def tag_params
       params.permit(:tag_name)
-    end
-
-    def extract_tags(tags_param)
-      tags = tags_param.presence || ""
-      tags.split(",").map(&:strip)
     end
 
     # 以下、before action
--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,6 @@
 module RecordsHelper
+    def extract_tags(tags)
+        tags = tags.presence || ""
+        tags.split(",").map(&:strip)
+    end
 end
